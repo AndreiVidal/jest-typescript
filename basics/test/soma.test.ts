285655c4c0e79aafc8ea7a20de73214a977dcf14
Andrei Vidal <andrei.godoi92@gmail.com>
2025-12-07 19:24:58 -0300

    My first test in jest

--- basics/test/soma.test.ts
+++ basics/test/soma.test.ts
@@ -1,6 +1,17 @@
 import { soma } from "../src/soma";
 
-test("Deve testar a função soma",()=>{
-    const result = soma(1,2)
+test("Deve testar a função soma", () => {
+    const result = soma(1, 2)
     expect(result).toBe(3)
 })
+
+
+test("Deve retornar a soma de números positivos", () => {
+    expect(soma(2, 3)).toBe(5)
+})
+
+test("Deve retornar a soma de números negativo", () => {
+    expect(soma(-2, -3)).toBe(-5)
+})
+
+
